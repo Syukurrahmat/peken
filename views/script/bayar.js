398f39e62bcee60e6533636a41c5667b7cf99a11
syukurrahmat <syukurtullah@gmail.com>
2021-06-16 20:06:58 +0700

    fix

--- views/script/bayar.js
+++ views/script/bayar.js
@@ -1,3 +1,14 @@
+function toFullTanggal(dt){
+    let bulanArr = ['Januari', 'Februari', 'Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November','Desember'];
+    let date = new Date(new Date(dt).toISOString()).toLocaleString()
+
+    let tanggal = date.split(' ')[0].split('/')
+    tanggal[1] = bulanArr[tanggal[1]-1]
+    tanggal = tanggal.join(' ')
+    let jam = date.split(' ')[1].split('.').slice(0,2).join(':')
+    return (`${jam} , ${tanggal}`);
+
+}
 
 let deadline = document.querySelector('.deadline')
 deadline.innerHTML = toFullTanggal(deadline.getAttribute('data-dt'))
